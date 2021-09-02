7bcc94d7cbc2e2013f1d5e51c85642b9a4174ba3
lisiyu <lisiyu_bit@163.com>
2021-09-02 21:30:52 +0800

    v3

--- src/main.go
+++ src/main.go
@@ -21,13 +21,16 @@ func main() {
 	 */
 	// the range of key: A-Z
 
-	trie := mpt.NewTrie()
-	trie.Insert([]byte{'A', 'B', 'C'}, "hello")
-	trie.Insert([]byte{'A', 'B', 'C'}, "world")
-	v1, f1 := trie.GetExactOne([]byte{'A', 'B', 'C', 'D'})
-	v2, f2 := trie.GetExactOne([]byte{'A', 'B', 'C'})
-	fmt.Println(v1, f1)
-	fmt.Println(v2, f2)
+	//trie := mpt.NewTrie()
+	//trie.Insert([]byte{'A', 'B', 'C'}, "hello")
+	//trie.Insert([]byte{'A', 'B', 'C'}, "world")
+	//v1, f1 := trie.GetExactOne([]byte{'A', 'B', 'C', 'D'})
+	//v2, f2 := trie.GetExactOne([]byte{'A', 'B', 'C'})
+	//fmt.Println(v1, f1)
+	//fmt.Println(v2, f2)
+
+	bo, i := mpt.ContainJudge([]byte("ABBCDEEFFGH"), []byte("ZZZZZ"))
+	fmt.Println(bo, i)
 
 
 
--- src/mpt/trie.go
+++ src/mpt/trie.go
@@ -10,8 +10,9 @@ func NewTrie() *Trie {
 
 func (t *Trie) Insert(key []byte, value string) error {
 	/*
-		Inserting (key, value) into trie
-		key: the key to be inserted
+	Inserting (key, value) into trie
+	key: the key to be inserted
+	value: the value to be inserted
 	*/
 
 	node := &t.root
@@ -154,19 +155,70 @@ func (t *Trie) GetExactOne(key []byte) ([]string, bool){
 	}
 }
 
-func (t *Trie) GetCandidate(key []byte) (map[string][]string, bool){
+type branch struct {
+	key []byte
+	node Node
+}
+func (t *Trie) GetCandidate(key []byte) ([]string, bool){
 	/*
 	get results that include given key
 	 */
 
-	node := t.root
-	result := make(map[string][]string)
-	for {
-		if IsEmptyNode(node) {
-			return result, false
-		}
+	var result []string
+	if root, ok := t.root.(*BranchNode); ok {
+		node := root.GetBranch(key[0])
+		key = key[1:]
+		var stack []branch
+		stack = append(stack, branch{key, node})
+		for {
+			if IsEmptyNode(node) {
+				return result, false
+			}
 
+			if leaf, ok := node.(*LeafNode); ok {
+				matched := PrefixMatchedLen(leaf.Path, key)
+				if matched == len(key) || IsContain(leaf.Path[matched:], key[matched:]){
+					result = append(result, leaf.Value...)
+				}
+				// next branch
+				continue
+			}
+
+			//if branch, ok := node.(*BranchNode); ok {
+			//
+			//}
+
+			if ext, ok := node.(*ExtensionNode); ok {
+				matched := PrefixMatchedLen(ext.Path, key)
+				if matched < len(ext.Path) && matched < len(key){
+					if ext.Path[len(ext.Path)-1] < key[matched] {
+						key = key[matched:]
+						node = ext.Next
+						continue
+					} else {
+						containAll, i := ContainJudge(ext.Path[matched:], key[matched:])
+						if containAll{
+							key = []byte{}
+							node = ext.Next
+							continue
+						} else if ext.Path[len(ext.Path)-1] < key[i] {
+							key = key[i:]
+							node = ext.Next
+							continue
+						} else {
+							// next branch
+							continue
+						}
+					}
+				} else {
+					key = key[matched:]
+					node = ext.Next
+					continue
+				}
+			}
+		}
 	}
+	return result, false
 }
 
 func (t *Trie) Print() {
@@ -186,4 +238,52 @@ func PrefixMatchedLen(node1, node2 []byte) int {
 	return matched
 }
 
+func IsContain(node1, node2 []byte) bool {
+	/*
+	Judge the key of node1 whether contain the key of the node2
+	*/
 
+	for _, v := range node1 {
+		if len(node2) == 0 {
+			return true
+		} else {
+			if v > node2[0] {
+				return false
+			} else if v == node2[0] {
+				node2 = node2[1:]
+				continue
+			}
+		}
+	}
+	if len(node2) != 0 {
+		return false
+	} else {
+		return true
+	}
+}
+
+func ContainJudge(node1, node2 []byte) (bool, int) {
+	/*
+	Judge the key of node1 whether contain the key of the node2, if not, return the position in node2
+	 */
+
+	i := 0
+	for _, v := range node1 {
+		if len(node2) == 0 {
+			return true, -1
+		} else {
+			if v > node2[0] {
+				return false, i
+			} else if v == node2[0] {
+				i = i + 1
+				node2 = node2[1:]
+				continue
+			}
+		}
+	}
+	if len(node2) != 0 {
+		return false, i
+	} else {
+		return true, -1
+	}
+}
