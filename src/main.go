f3dc2b0fba8d9387cfd7be1025d70c1187c993e7
lisiyu <lisiyu_bit@163.com>
2022-03-14 14:44:49 +0800

    latest

--- src/main.go
+++ src/main.go
@@ -3,47 +3,51 @@ package main
 import (
 	"Corgi/src/matching"
 	"Corgi/src/mpt"
+	"Corgi/src/verification"
 	"fmt"
+	"time"
 )
 
 func main(){
 
 	fmt.Println("----------------Loading Graph----------------")
 	g := new(matching.Graph)
-	dataset := "wn"
+	dataset := "ex"
 	switch dataset {
 	case "ex":
 		g.LoadUnGraphFromTxt("./data/example1.txt")
 		g.AssignLabel("./data/example1_label.txt")
-		g.ObtainPathFeature("./data/pf/JExample.json")
+		//g.ObtainPathFeature("./data/pf2/JExample.json")
 	case "ye":
 		g.LoadUnGraphFromTxt("./data/yeast.txt")
 		g.AssignLabel("./data/yeast_label.txt")
-		g.ObtainPathFeature("./data/pf/JYeast.json")
+		//g.ObtainPathFeature("./data/pf2/JYeast.json")
 	case "hu":
 		g.LoadUnGraphFromTxt("./data/human.txt")
 		g.AssignLabel("./data/human_label.txt")
-		g.ObtainPathFeature("./data/pf/JHuman.json")
+		g.ObtainPathFeature("./data/pf2/JHuman.json")
 	case "wn":
 		g.LoadUnGraphFromTxt("./data/wordnet.txt")
 		g.AssignLabel("./data/wordnet_label.txt")
-		g.ObtainPathFeature("./data/pf/JWordnet.json")
+		g.ObtainPathFeature("./data/pf2/JWordnet.json")
 	case "db":
 		g.LoadUnGraphFromTxt("./data/dblp.txt")
 		g.AssignLabel("./data/dblp_label.txt")
-		g.ObtainPathFeature("./data/pf/JDblp.json")
+		g.WritePathFeature("./data/pf2/JDblp.json")
+		//g.ObtainPathFeature("./data/pf2/JDblp.json")
 	case "am":
 		g.LoadUnGraphFromTxt("./data/amazon.txt")
 		g.AssignLabel("./data/amazon_label.txt")
-		g.ObtainPathFeature("./data/pf/JAmazon.json")
+		g.WritePathFeature("./data/pf2/JAmazon.json")
+		//g.ObtainPathFeature("./data/pf2/JAmazon.json")
 	case "yt":
 		g.LoadUnGraphFromTxt("./data/youtube.txt")
 		g.AssignLabel("./data/youtube_label.txt")
-		g.ObtainPathFeature("./data/pf/JYoutube.json")
+		//g.ObtainPathFeature("./data/pf2/JYoutube.json")
 	case "lj":
-		g.LoadUnGraphFromTxt("./data/livejournal.txt")
-		g.AssignLabel("./data/livejournal_label.txt")
-		g.ObtainPathFeature("./data/pf/JLivejournal.json")
+		g.LoadUnGraphFromTxt("../data/livejournal.txt")
+		g.AssignLabel("../data/livejournal_label.txt")
+		//g.ObtainPathFeature("./data/pf2/JLivejournal.json")
 	}
 
 	fmt.Println("----------------Building MVPTree----------------")
@@ -54,7 +58,8 @@ func main(){
 			trie.Insert(byteKey, e, g.NeiHashes[e], g.Vertices[e].Content)
 		}
 	}
-	//RD := trie.HashRoot()
+	RD := trie.HashRoot()
+	fmt.Println(RD)
 
 	fmt.Println("----------------Loading Query----------------")
 	var q matching.QueryGraph
@@ -74,62 +79,42 @@ func main(){
 		q = matching.LoadProcessing("./data/query6.txt", "./data/query6_label.txt")
 	}
 
-	fmt.Println("----------------Test for candidate set filtering----------------")
-	CS := make(map[int][]int)
-	CSF := make(map[int][]int)
+	//fmt.Println("-----------------the rate of remove false positive------------------")
+	//trie.AuthFilter(&q)
+	//sum1 := 0
+	//for _, c := range q.CandidateSets {
+	//	sum1 = sum1 + len(c)
+	//}
+	//fmt.Println(sum1)
+	//trie.AuthenFilterPlus(&q, *g)
+	//sum2 := 0
+	//for _, c := range q.CandidateSets {
+	//	sum2 = sum2 + len(c)
+	//}
+	//fmt.Println(sum2)
 
-	for str, ul := range q.NeiStr {
-		fmt.Println("present key: ", str)
-		C := trie.GetCandidate([]byte(str))
-		for _, u := range ul {
-			CS[u] = C
-		}
-	}
-	total := 0
-	for k, lis := range CS {
-		fmt.Println(k, len(lis))
-		total = total + len(lis)
-	}
-	fmt.Println(total)
-	fmt.Println("after filter: ")
-	for k, c := range CS {
-		for _, v := range c {
-			flag := true
-			for pa, num := range q.PathFeature[k] {
-				if _, ok := g.PathFeature[v][pa]; !ok {
-					flag = false
-					break
-				} else if len(g.PathFeature[v][pa]) < num {
-					flag = false
-					break
-				}
-			}
-			if flag {
-				CSF[k] = append(CSF[k], v)
-			}
-		}
-	}
-	RealSum := 0
-	for k, lis := range CSF {
-		fmt.Println(k, len(lis))
-		RealSum = RealSum + len(lis)
-	}
-	fmt.Println(RealSum)
+	startT1 := time.Now()
+	fmt.Println("----------------Authenticated Filtering----------------")
+	VO := verification.VO{}
+	VO.NodeList = trie.AuthFilter(&q)
+	//VO.NodeList = trie.AuthenFilterPlus(&q, *g)
+	time1 := time.Since(startT1)
+	fmt.Println("phase 1 SP CPU time is: ", time1)
 
+	startT2 := time.Now()
+	fmt.Println("----------------Authenticated Matching----------------")
+	VO2 := g.AuthMatching(q)
+	VO.CSG = VO2.CSG
+	VO.FP = VO2.FP
+	VO.RS = VO2.RS
+	time2 := time.Since(startT2)
+	fmt.Println("phase 2 SP CPU time is: ", time2)
 
-	//fmt.Println("----------------Authenticated Filtering----------------")
-	//VO := verification.VO{}
-	//VO.NodeList = trie.AuthFilter(&q)
-	//fmt.Println(q.CandidateSets)
-	//
-	//fmt.Println("----------------Authenticated Matching----------------")
-	//VO2 := g.AuthMatching(q)
-	//VO.CSG = VO2.CSG
-	//VO.FP = VO2.FP
-	//VO.RS = VO2.RS
-	//
-	//fmt.Println("----------------Verification----------------")
-	//F, _ := VO.Authentication(q, RD)
-	//fmt.Println(F)
+	startT3 := time.Now()
+	fmt.Println("----------------Verification----------------")
+	F, _ := VO.Authentication(q, RD)
+	fmt.Println(F)
+	time3 := time.Since(startT3)
+	fmt.Println("Client CPU time is: ", time3)
 
 }
